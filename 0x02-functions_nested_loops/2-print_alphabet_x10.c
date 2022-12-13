309c65ec158a20d2b989c38985d162dd8ae384ec
C-Nmachi <nmachiciaran@gmail.com>
2022-12-13 12:04:25 -0800

    ten times the alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+lude "main.h"
+
+/**
+ * print_alphabet_x10 - check main
+ * Description: _putchar to print alphabet in lowercase 10 times
+ * Return: Nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	char = n;
+	int = c;
+
+	for (c = 0; c < 10; c++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+			_putchar(n);
+		_putchar('\n');
+	}
+
+}
