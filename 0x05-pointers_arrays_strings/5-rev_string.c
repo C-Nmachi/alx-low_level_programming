c8f4bece95641f30fb169e3eab03bd38e7efe499
C-Nmachi <nmachiciaran@gmail.com>
2022-12-19 16:23:50 -0800

    correction

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- * rev_string - reverses a string.
- * @s: input string.
- * Return: no return.
- */
+* rev_string - reverses a string.
+* @s: input string.
+* Return: no retun.
+*/
 void rev_string(char *s)
 {
 	int count = 0, i, j;
