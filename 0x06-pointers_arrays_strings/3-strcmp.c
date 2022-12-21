af00f6969f65b1d2dff488d7dc2c7cd1919fc43b
C-Nmachi <nmachiciaran@gmail.com>
2022-12-21 13:18:31 -0800

    correction

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,14 +6,12 @@
 *@n: number of elements of an array.
 *Return: void.
 */
-
 void reverse_array(int *a, int n)
 {
 	int temp, counter;
 
 	n = n - 1;
 	counter = 0;
-
 	while (counter <= n)
 	{
 		temp = a[counter];
